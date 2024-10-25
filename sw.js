d6fbd1b2f39778de9f39c5d18d2ae62a9cd0997a
GitAndreaHub <74855697+GitAndreaHub@users.noreply.github.com>
2024-10-25 13:39:10 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,25 @@
+
+// Service Worker
+const CACHE_NAME = 'giococoro-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  'https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.2.19/tailwind.min.css',
+  'https://cdnjs.cloudflare.com/ajax/libs/react/18.2.0/umd/react.production.min.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/react-dom/18.2.0/umd/react-dom.production.min.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/babel-standalone/7.23.5/babel.min.js'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
